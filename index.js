71ca805f94232506509bf0f8de9a33380c4db02b
ermondel <ermondel@gmail.com>
2018-09-22 10:00:30 +0300

    feat: Disable previous drawings

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const pageLoaded = () => {
     const ctx = canvas.getContext('2d');
 
     //
-    ctx.fillStyle = "lightgreen";
+    // ctx.fillStyle = "lightgreen";
     // Filled rectangles
-    ctx.fillRect(200, 10, 100, 100);
-    ctx.fillRect(50, 70, 90, 30);
+    // ctx.fillRect(200, 10, 100, 100);
+    // ctx.fillRect(50, 70, 90, 30);
     // Stroked rectangles
-    ctx.strokeRect(110, 10, 50, 50);
-    ctx.strokeRect(30, 10, 50, 50);
+    // ctx.strokeRect(110, 10, 50, 50);
+    // ctx.strokeRect(30, 10, 50, 50);
     // Clearing rectangles
-    ctx.clearRect(210, 20, 30, 20);
-    ctx.clearRect(260, 20, 30, 20);
+    // ctx.clearRect(210, 20, 30, 20);
+    // ctx.clearRect(260, 20, 30, 20);
 
     // 1
-    ctx.fillStyle = "orange";
-    ctx.fillRect(330, 10, 49, 49);
-    ctx.fillRect(381, 10, 49, 49);
-    ctx.fillRect(330, 61, 49, 49);
-    ctx.fillRect(381, 61, 49, 49);
+    // ctx.fillStyle = "orange";
+    // ctx.fillRect(330, 10, 49, 49);
+    // ctx.fillRect(381, 10, 49, 49);
+    // ctx.fillRect(330, 61, 49, 49);
+    // ctx.fillRect(381, 61, 49, 49);
+
+    // 2
+
 }
