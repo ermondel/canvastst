81cbebe5bc2dd8681d6f6d4c2698986f43b7a442
ermondel <ermondel@gmail.com>
2018-10-16 18:11:57 +0300

    aquamarine quadrilateral, brown polygon, purple circle

--- data/index.js
+++ data/index.js
@@ -22,4 +22,22 @@ const pageLoaded = () => {
     ctx.arc(325, 75, 50, 0, 2*Math.PI, true);
     ctx.fill();
 
+    //
+
+    ctx.fillStyle = 'aquamarine';
+    ctx.fillRect(10, 150, 240, 40);
+
+    ctx.fillStyle = 'brown';
+    ctx.beginPath();
+    ctx.moveTo(300, 150);
+    ctx.lineTo(380, 170);
+    ctx.lineTo(360, 230);
+    ctx.lineTo(255, 180);
+    ctx.fill();
+
+    ctx.fillStyle = 'purple';
+    ctx.beginPath();
+    ctx.arc(450, 180, 45, 0, 2*Math.PI, true);
+    ctx.fill();
+    
 };
