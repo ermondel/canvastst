f90c8d701b26dcca6de7d82b2465cf4333db235f
ermondel <ermondel@gmail.com>
2018-11-11 19:14:39 +0200

    feat: Move old code to archive

--- data/archive.js
+++ data/archive.js
@@ -1,7 +1,7 @@
 const pageLoaded = () => {
-    console.log('---', 'page loaded');
-    const canvas = document.getElementById('testcanvas');
-    const ctx = canvas.getContext('2d');
+    // console.log('---', 'page loaded');
+    // const canvas = document.getElementById('testcanvas');
+    // const ctx = canvas.getContext('2d');
 
     //
     // ctx.fillStyle = "lightgreen";
@@ -90,15 +90,53 @@ const pageLoaded = () => {
     // ctx.drawImage(spaceship, 0, 0, 60, 50, 0, 0, 60, 50);
 
     // Rotation and translation
-    const spaceship = document.getElementById('spaceship');
-    ctx.translate(250, 370);
-    ctx.rotate(Math.PI / 3);
-    ctx.drawImage(spaceship, 0, 0, 60, 50, -30, -25, 60, 50);
-    ctx.rotate(-Math.PI / 3);
-    ctx.translate(-240, -370);
-    ctx.translate(300, 370);
-    ctx.rotate(3 * Math.PI / 4);
-    ctx.drawImage(spaceship, 0, 0, 60, 50, -30, -25, 60, 50);   
-    ctx.rotate(-3 * Math.PI / 4);
-    ctx.translate(-300, -370);
+    // const spaceship = document.getElementById('spaceship');
+    // ctx.translate(250, 370);
+    // ctx.rotate(Math.PI / 3);
+    // ctx.drawImage(spaceship, 0, 0, 60, 50, -30, -25, 60, 50);
+    // ctx.rotate(-Math.PI / 3);
+    // ctx.translate(-240, -370);
+    // ctx.translate(300, 370);
+    // ctx.rotate(3 * Math.PI / 4);
+    // ctx.drawImage(spaceship, 0, 0, 60, 50, -30, -25, 60, 50);   
+    // ctx.rotate(-3 * Math.PI / 4);
+    // ctx.translate(-300, -370);
+
+    // const canvas = document.getElementById('testcanvas');
+    // const ctx = canvas.getContext('2d');
+
+    // ctx.fillStyle = 'lightgreen';
+    // ctx.fillRect(25, 25, 100, 100);
+
+    // ctx.fillStyle = 'orange';
+    // ctx.strokeStyle = 'orange';
+    // ctx.beginPath();
+    // ctx.moveTo(150, 25);
+    // ctx.lineTo(150, 125);
+    // ctx.lineTo(250, 75);
+    // ctx.fill();
+
+    // ctx.fillStyle = 'blue';
+    // ctx.strokeStyle = 'blue';
+    // ctx.beginPath();
+    // ctx.arc(325, 75, 50, 0, 2*Math.PI, true);
+    // ctx.fill();
+
+    //
+
+    // ctx.fillStyle = 'aquamarine';
+    // ctx.fillRect(10, 150, 240, 40);
+
+    // ctx.fillStyle = 'brown';
+    // ctx.beginPath();
+    // ctx.moveTo(300, 150);
+    // ctx.lineTo(380, 170);
+    // ctx.lineTo(360, 230);
+    // ctx.lineTo(255, 180);
+    // ctx.fill();
+
+    // ctx.fillStyle = 'purple';
+    // ctx.beginPath();
+    // ctx.arc(450, 180, 45, 0, 2*Math.PI, true);
+    // ctx.fill();
 }
--- data/index.js
+++ data/index.js
@@ -1,43 +1,5 @@
 const pageLoaded = () => {
 
     console.log('|', 'page loaded', '|');
-
-    const canvas = document.getElementById('testcanvas');
-    const ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = 'lightgreen';
-    ctx.fillRect(25, 25, 100, 100);
-
-    ctx.fillStyle = 'orange';
-    ctx.strokeStyle = 'orange';
-    ctx.beginPath();
-    ctx.moveTo(150, 25);
-    ctx.lineTo(150, 125);
-    ctx.lineTo(250, 75);
-    ctx.fill();
-
-    ctx.fillStyle = 'blue';
-    ctx.strokeStyle = 'blue';
-    ctx.beginPath();
-    ctx.arc(325, 75, 50, 0, 2*Math.PI, true);
-    ctx.fill();
-
-    //
-
-    ctx.fillStyle = 'aquamarine';
-    ctx.fillRect(10, 150, 240, 40);
-
-    ctx.fillStyle = 'brown';
-    ctx.beginPath();
-    ctx.moveTo(300, 150);
-    ctx.lineTo(380, 170);
-    ctx.lineTo(360, 230);
-    ctx.lineTo(255, 180);
-    ctx.fill();
-
-    ctx.fillStyle = 'purple';
-    ctx.beginPath();
-    ctx.arc(450, 180, 45, 0, 2*Math.PI, true);
-    ctx.fill();
     
 };
